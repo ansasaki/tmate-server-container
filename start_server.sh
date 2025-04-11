918fed36945e990fc075ae90e7d76b268abe8c6d
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2025-04-11 13:45:32 +0200

    Cleanup messages printed by the container to the console
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-create_keys
+create_keys >> /dev/null
 print_tmate_conf >> /dev/stdout
+echo "" >> /dev/stdout
 tmate-ssh-server -k "${SSH_KEY_DIR}" -p "${SSH_PORT_LISTEN}" -h "${SSH_HOSTNAME}"
